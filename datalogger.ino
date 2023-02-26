c7e78f24015311ab9b09bece57cc3312f18de395
pr414 <54544052+pr414@users.noreply.github.com>
2023-02-26 20:32:50 +0100

    MQTT integration
    Static Certificate Test

--- datalogger.ino
+++ datalogger.ino
@@ -36,7 +36,7 @@ COBDSPI obd;
 //PID OBD di cui fare il Logging
 const int numOfPIDs = 7;
 byte pids[]= {PID_RPM, PID_SPEED, PID_THROTTLE, PID_HYBRID_BATTERY_PERCENTAGE, PID_ENGINE_TORQUE_DEMANDED, PID_ENGINE_REF_TORQUE, PID_ENGINE_TORQUE_PERCENTAGE};
-char* stamps[]= {"rpm", "speed", "throttle", "battery_perc", "torque_demanded", "torque_reference", "torque_erogated"};
+char *stamps[7]= {"rpm", "speed", "throttle", "battery_perc", "torque_demanded", "torque_reference", "torque_erogated"};
 int values[7] = {0,0,0,0,0,0,0};
 
 //JSON Document
@@ -45,7 +45,7 @@ DynamicJsonDocument jsondoc(capacity);
 
 //Credenziali di accesso all'Hotspot
 const char* ssid = "OnePlus-Paolo";
-const char* password = "Woahcarlo$#";
+const char* wifipassword = "Woahcarlo$#";
 
 //Indirizzo e parametri del Broker MQTT
 const char* client_name = "PRivaClient";
@@ -53,8 +53,36 @@ const char* mqtt_server = "lab-elux.unibs.it";
 const int port = 50009;
 const char* topicToPublish = "/telemetry/obd/car1";
 const char* username = "priva";
-const char* password = "KObM2u96t%&M#e%ShZ#H!5Ls$0UEN^wXLuegI@*1rudAPeQE"; //CHECK %
-const char* certificate_path = "intermediate_ca.pem";
+const char* password = "KObM2u96t%&M#e%%ShZ#H!5Ls$0UEN^wXLuegI@*1rudAPeQE"; //CHECK %
+const char* certificate_path = "../intermediate_ca.pem";
+const char *x509CA PROGMEM = R"EOF("
+-----BEGIN CERTIFICATE-----
+MIIENjCCAx6gAwIBAgIUGFQHYrPCIetdZ2Qy2cUrSeuX/n0wDQYJKoZIhvcNAQEL
+BQAwgaAxCzAJBgNVBAYTAklUMRAwDgYDVQQHEwdCcmVzY2lhMR4wHAYDVQQKExVV
+bml2ZXJzaXR5IG9mIEJyZXNjaWExQDA+BgNVBAsTN2VMVVggTGFib3JhdG9yeSAt
+IERlcGFydG1lbnQgb2YgSW5mb3JtYXRpb24gRW5naW5lZXJpbmcxHTAbBgNVBAMT
+FGVMVVgtSW50ZXJtZWRpYXRlLUNBMB4XDTE4MDYwODA5MzYwMFoXDTIzMDQyMzA5
+MzYwMFowgaAxCzAJBgNVBAYTAklUMRAwDgYDVQQHEwdCcmVzY2lhMR4wHAYDVQQK
+ExVVbml2ZXJzaXR5IG9mIEJyZXNjaWExQDA+BgNVBAsTN2VMVVggTGFib3JhdG9y
+eSAtIERlcGFydG1lbnQgb2YgSW5mb3JtYXRpb24gRW5naW5lZXJpbmcxHTAbBgNV
+BAMTFGVMVVgtSW50ZXJtZWRpYXRlLUNBMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
+MIIBCgKCAQEAvKpFuiUfCBrvTFzEZR3E6xjAUyvo02ZxP1HT3kHEcaU3BRB/CBL3
+pVbDxemwkBlXKjHMfmiCpRX3aVSgR7qgNRjuMgG9XrNXpapMPIzEQas6m/2vSHZ+
+3gMIUemz8uLFNQFWcYNAYWU+cYaOcKYdL3EKLxCd8mE3ufkZoWBoYvV33/Ef8aox
+3X+JYb0J5H1qGLje4dvtfcIdAOXUO81K3XQgdi1VGE3KOdhhsjYTtEbKrQS5P9yO
+HLpNDvJctLZWEBjR5kdsux21TCXqrbVF3whrPWQJVuVZYbnRkVpmrMvX1Lbnwk0V
+1YuKllU1iG1jpgaEwNqjTzxVdfvUylwxGQIDAQABo2YwZDAOBgNVHQ8BAf8EBAMC
+AQYwEgYDVR0TAQH/BAgwBgEB/wIBAjAdBgNVHQ4EFgQU7iyn4WKaJ0lyxpNPAht6
+Pc1EKSYwHwYDVR0jBBgwFoAU7iyn4WKaJ0lyxpNPAht6Pc1EKSYwDQYJKoZIhvcN
+AQELBQADggEBAE3kbrlrPpeCFfOvZQzx2fpHsjCqhG904Tj9rg4idP+UmzSsgcn8
+wGy5UFrYkyRAAryuPhAS47Uefigkk4mVIslU7Dl3xxAw2TCpTZs5eTOExLGAY0lH
+g6ff3e/8iRwWl8DS/yx1WFQUeLYVK7da24H7zyow4TELue+QjabsHL7dQlK7bfbI
+r4nca5jejPNDTwK4D3zoQP9xBDqflJatjuU9/3MjqUKXh4RTXuEQpj+N16VgEsMp
+BUzevFCF+JJfJW/AtmchNh8YvS2EkXIpdi3y1brkzj4wNxKYQ3AMY4E6D9SWbhbe
+YhAvMkixXS8TJE2JNWTLHW+lbk1euAU79Qs=
+-----END CERTIFICATE-----
+")EOF";
+char* PROGMEM mqtt_certificate = "";
 //FILE *ca = fopen(ca_certificate, "rb");
 
 //Variabili per WiFi/MQTT
@@ -75,7 +103,7 @@ void setup_wifi() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, wifipassword);
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
@@ -116,7 +144,7 @@ void setup_mqtt() {
   //Lettura Certificato CA in "certificate"
     std::ifstream file(certificate_path);
     std::string str;
-    std::string PROGMEM certificate; //Specifico che voglio che il file rimanga salvato nella memoria flash con PROGMEM invece che nella RAM
+    std::string certificate; //Specifico che voglio che il file rimanga salvato nella memoria flash con PROGMEM invece che nella RAM
 
     while (std::getline(file, str)) {
       certificate += str;
@@ -124,7 +152,12 @@ void setup_mqtt() {
     }  
 
     //Setting del Certificato SSL
-    espClient.setCACert(certificate.c_str());
+    mqtt_certificate = const_cast<char*>(certificate.c_str());
+    Serial.println();
+    Serial.println("CERTIFICATO LETTO:");
+    Serial.println(x509CA);
+    Serial.println();
+    espClient.setCACert(x509CA);
 
     //Setting del Server MQTT e Callback sul topic
     client.setServer(mqtt_server, port);
@@ -229,7 +262,7 @@ void loop() {
     reconnect(); //Connette o Riconnette il Client al Broker specificato
   }
   client.loop(); //Processing In-Out of the MQTT Data
-  
+
 
   //Inserimento dati nel JSON Document Pre-Serializzazione
   jsondoc["timestamp"] = timestamp;
